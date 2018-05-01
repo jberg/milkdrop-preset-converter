b5b2a7ee92c154fed5d1834acfa8acc7abea09c6
jberg <jordannealberg@gmail.com>
2018-04-30 18:11:57 -0700

    add script output

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -6,19 +6,20 @@ const path = require('path');
 const env  = require('yargs').argv.env;
 
 const srcRoot = path.join(__dirname, '..', 'src');
+const scriptRoot = path.join(__dirname, '..', 'script');
 const nodeRoot = path.join(__dirname, '..', 'node_modules');
 const outputPath = path.join(__dirname, '..', 'dist');
 
-let plugins = [];
 let outputFile = 'md-preset-converter';
 
 if (env === 'prod') {
   outputFile += '.min';
 }
 
-let config = {
+let clientConfig = {
   entry: srcRoot + '/index.js',
   devtool: 'source-map',
+  target: 'web',
   output: {
     path: outputPath,
     filename: outputFile + '.js',
@@ -60,7 +61,7 @@ let config = {
 
 
 if (env === 'prod') {
-  config.plugins.push(
+  clientConfig.plugins.push(
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
@@ -74,4 +75,64 @@ if (env === 'prod') {
   );
 }
 
-module.exports = config;
+
+let scriptOutputFile = 'md-preset-converter-node';
+
+if (env === 'prod') {
+  scriptOutputFile += '.min';
+}
+
+let scriptConfig = {
+  entry: scriptRoot + '/index.js',
+  target: 'node',
+  output: {
+    path: outputPath,
+    filename: scriptOutputFile + '.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.js)$/,
+        exclude: /node_modules|lib/,
+        use: {
+          loader: 'babel-loader?cacheDirectory',
+          options: {
+            plugins: ['transform-runtime'],
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /(\.js)$/,
+        exclude: /node_modules|lib/,
+        use: {
+          loader: 'eslint-loader'
+        },
+        enforce: 'pre'
+      },
+    ]
+  },
+  resolve: {
+    modules: [scriptRoot, srcRoot, nodeRoot],
+    extensions: ['.js']
+  },
+  plugins: []
+};
+
+
+if (env === 'prod') {
+  scriptConfig.plugins.push(
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+
+    new UglifyJsPlugin()
+  );
+}
+
+module.exports = [clientConfig, scriptConfig];

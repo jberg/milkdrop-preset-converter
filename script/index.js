7288e946006cab1e2f26a30c278bf6283b291d6b
jberg <jordannealberg@gmail.com>
2018-05-01 13:48:20 -0700

    process shaders after glsl-optimizer

--- script/index.js
+++ script/index.js
@@ -5,6 +5,34 @@ import spawnPromiseWithInput from './spawnPromise';
 import { prepareShader } from '../src/shaders';
 import { splitPreset, createBasePresetFuns } from '../src/preset';
 
+function processShader (shader) {
+  if (_.isEmpty(shader)) {
+    return '';
+  }
+
+  let processedShader = _.replace(shader, 'shader_body', 'xlat_main');
+  return processedShader;
+}
+
+function processConvertedShader (shader) {
+  if (_.isEmpty(shader)) {
+    return '';
+  }
+
+  let processedShader = _.replace(shader, '#version 300 es\n', '');
+  processedShader = _.replace(processedShader, 'void main ()', 'shader_body');
+  processedShader = _.replace(processedShader, /xlv_TEXCOORD0/g, 'uv');
+  processedShader = _.replace(processedShader, '_glesFragData[0]', 'ret');
+  processedShader = _.replace(processedShader, 'out lowp vec4 _glesFragData[4];\n', '');
+  processedShader = _.replace(processedShader, /highp\s*/g, '');
+  processedShader = _.replace(processedShader, /medp\s*/g, '');
+  processedShader = _.replace(processedShader, /lowp\s*/g, '');
+  processedShader = _.join(_.filter(_.split(processedShader, '\n'), (line) => {
+    return !(_.startsWith(line, 'in') || _.startsWith(line, 'uniform'));
+  }), '\n');
+  return processedShader;
+}
+
 const args = process.argv.slice(2);
 
 if (args.length < 3) {
@@ -24,22 +52,18 @@ const presetMap = createBasePresetFuns(presetParts.presetInit,
                                        presetParts.shapes,
                                        presetParts.waves);
 
-let warpShader = prepareShader(presetParts.warp);
-warpShader = _.replace(warpShader, 'shader_body', 'xlat_main');
-
-let compShader = prepareShader(presetParts.comp);
-compShader = _.replace(compShader, 'shader_body', 'xlat_main');
-
+const warpShader = processShader(prepareShader(presetParts.warp));
+const compShader = processShader(prepareShader(presetParts.comp));
 const hlslconvWarp = spawnPromiseWithInput(warpShader, args[0]);
 const hlslconvComp = spawnPromiseWithInput(compShader, args[0]);
 
 Promise.all([hlslconvWarp, hlslconvComp])
 .then((shaders) => {
-  const presetOutput = _.assign({}, presetMap, {
-    baseVals: presetParts.baseVals,
-    warp: shaders[0].toString(),
-    comp: shaders[1].toString(),
+  const presetOutput = _.assign({ baseVals: presetParts.baseVals }, presetMap, {
+    warp: processConvertedShader(shaders[0].toString()),
+    comp: processConvertedShader(shaders[1].toString()),
   });
+
   fs.writeFileSync(`${args[2]}/${presetOutputName}`, JSON.stringify(presetOutput));
 })
 .catch((err) => {

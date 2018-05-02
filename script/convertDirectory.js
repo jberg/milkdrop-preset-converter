7362d3ac3d30555a166060dd58f3154bc4f501d5
jberg <jordannealberg@gmail.com>
2018-05-01 20:53:51 -0700

    convert directory script

---
+++ script/convertDirectory.js
@@ -0,0 +1,22 @@
+const fork = require('child_process').fork;
+const fs = require('fs');
+
+const args = process.argv.slice(2);
+
+function convertPreset (item) {
+  return new Promise((resolve, reject) => {
+    const cp = fork(args[0], [args[1], `${args[2]}/${item}`, args[3]]);
+    cp.on('error', reject)
+      .on('close', (code) => (code === 0) ? resolve() : reject());
+  });
+}
+
+fs.readdir(args[2], async (err, items) => {
+  for (var i = 0; i < items.length; i++) {
+    const item = items[i];
+    if (item.endsWith('.milk')) {
+      console.log('converting %O', item);
+      await convertPreset(item);
+    }
+  }
+});

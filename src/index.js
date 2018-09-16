eeb71aff8a62ae65eb8576dd912c3776735edddf
jberg <jordannealberg@gmail.com>
2018-09-16 15:00:59 -0700

    add other preset conversion functions

--- src/index.js
+++ src/index.js
@@ -47,3 +47,36 @@ export async function convertPreset (text) {
     comp: compShader,
   });
 }
+
+export function convertPresetEquations (presetVersion, initEQs, frameEQs, pixelEQs) {
+  const parsedPreset = milkdropParser.convert_basic_preset(presetVersion, initEQs, frameEQs, pixelEQs);
+  return {
+    init_eqs_str: parsedPreset.perFrameInitEQs ? parsedPreset.perFrameInitEQs.trim() : '',
+    frame_eqs_str: parsedPreset.perFrameEQs ? parsedPreset.perFrameEQs.trim() : '',
+    pixel_eqs_str: parsedPreset.perPixelEQs ? parsedPreset.perPixelEQs.trim() : ''
+  };
+}
+
+export function convertWaveEquations (presetVersion, initEQs, frameEQs, pointEQs) {
+  const parsedPreset = milkdropParser.make_wave_map(presetVersion, {
+    init_eqs_str: initEQs,
+    frame_eqs_str: frameEQs,
+    point_eqs_str: pointEQs
+  });
+  return {
+    init_eqs_str: parsedPreset.perFrameInitEQs ? parsedPreset.perFrameInitEQs.trim() : '',
+    frame_eqs_str: parsedPreset.perFrameEQs ? parsedPreset.perFrameEQs.trim() : '',
+    point_eqs_str: parsedPreset.perPointEQs ? parsedPreset.perPointEQs.trim() : ''
+  };
+}
+
+export function convertShapeEquations (presetVersion, initEQs, frameEQs) {
+  const parsedPreset = milkdropParser.make_shape_map(presetVersion, {
+    init_eqs_str: initEQs,
+    frame_eqs_str: frameEQs
+  });
+  return {
+    init_eqs_str: parsedPreset.perFrameInitEQs ? parsedPreset.perFrameInitEQs.trim() : '',
+    frame_eqs_str: parsedPreset.perFrameEQs ? parsedPreset.perFrameEQs.trim() : ''
+  };
+}
